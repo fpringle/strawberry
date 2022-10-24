d79e02123c5826546f129ce4448f793ea9175eb1
fpringle <freddyjepringle@gmail.com>
2022-10-24 18:34:14 +0200

    change doc badge label

--- scripts/doc_badge.py
+++ scripts/doc_badge.py
@@ -4,7 +4,7 @@ import sys
 def make_badge(coverage, ofile):
     perc = int(coverage)
     color = "red" if perc < 50 else "orange" if perc < 75 else "green" if perc < 90 else "brightgreen"
-    b = Badge(left_txt="doc coverage", right_txt=f"{perc}%", color=color)
+    b = Badge(left_txt="documentation coverage", right_txt=f"{perc}%", color=color)
     b.write_to(ofile, use_shields=False)
 
 if __name__ == "__main__":
