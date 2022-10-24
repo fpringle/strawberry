f90b23579f06b47a8f39eb947bf07fa138acf761
fpringle <freddyjepringle@gmail.com>
2022-10-24 16:56:13 +0200

    github action: fix color bug; use latest version of setup-python

--- scripts/doc_badge.py
+++ scripts/doc_badge.py
@@ -3,7 +3,7 @@ import sys
 
 def make_badge(coverage, ofile):
     perc = int(coverage)
-    color = "red" if perc < 50 else "orange" if perc < 75 else "green" if perc < 90 else "bright green"
+    color = "red" if perc < 50 else "orange" if perc < 75 else "green" if perc < 90 else "brightgreen"
     b = Badge(left_txt="doc coverage", right_txt=f"{perc}%", color=color)
     b.write_to(ofile, use_shields=False)
 
