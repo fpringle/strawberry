738c937de8084871e9cc064e52bb50581055aae8
fpringle <freddyjepringle@gmail.com>
2022-10-26 10:09:58 +0200

    better doc action workflow

--- scripts/doc_coverage.sh
+++ scripts/doc_coverage.sh
@@ -2,4 +2,4 @@
 
 DIR=`dirname $0`
 ROOT=`dirname $DIR`
-python2 $DIR/doxy-coverage.py $ROOT/docs/xml | tail -n1 | grep -oe "^[0-9]\+"
+python2 $DIR/doxy-coverage.py $ROOT/doc/xml | tail -n1 | grep -oe "^[0-9]\+"
